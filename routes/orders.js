b3dd1e3d866a3368ee1a55a42549dfa44094fec7
José Carlos Haza <acremur@gmail.com>
2022-01-25 01:55:07 +0100

    Added Admin Section

--- routes/orders.js
+++ routes/orders.js
@@ -1,6 +1,35 @@
 const Order = require("../models/Order")
 const router = require('express').Router()
 
+router.get('/', async (req, res) => {
+    const orders = await Order.find({ 
+        isDelivered: false,
+        isCanceled: false
+    })
+    res.send(orders)
+})
+
+router.put('/:id', async (req, res) => {
+    const order = await Order.findById(req.params.id)
+
+    if (order) {
+        if (req.body.action === 'ready') {
+            order.isReady = true
+            order.inProgress = false
+        } else if (req.body.action === 'deliver') {
+            order.isDelivered = true
+        } else if (req.body.action === 'cancel') {
+            order.isCanceled = true
+        }
+
+        await order.save()
+        res.send({ message: 'Done' })
+    } else {
+        req.status(404).message({ message: 'Order not found' })
+    }
+    
+})
+
 router.post('/', async (req, res) => {
     const lastOrder = await Order.find().sort({ number: -1 }).limit(1)
     const lastNumber = lastOrder.length === 0 ? 0 : lastOrder[0].number
